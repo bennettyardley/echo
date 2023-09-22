54d9a1302f05c9fae140eff326ce3669c5c9a943
mjc5 <masoncooper85@gmail.com>
2023-09-22 19:02:40 -0400

    boom

--- frontend/src/components/NewEntryModal.vue
+++ frontend/src/components/NewEntryModal.vue
@@ -3,11 +3,15 @@
       <button class="btn btn-ghost btn-circle" @click="openModal">+</button>
       <dialog id="my_modal_3" class="modal">
         <div class="modal-box">
-          <form method="dialog">
+          <form method="dialog" class="content-center">
             <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" @click="closeModal">✕</button>
+            <h3 class="font-bold text-xl">Hello!</h3>
+            <p class="py-4">A Curse upon Your Bloodline</p>
+            <VueDatePicker :enable-time-picker="false" :max-date="new Date()" dark />
+            <v-select multiple taggable push-tags :options="allArtists" v-model="artists" class="text-primary-content rounded">Search or add an Artist</v-select>
+            <v-select taggable push-tags :options="allVenues" class="text-primary-content rounded">Search or add an Venue</v-select>
+            <button class="btn btn-primary mt-4 " @click="closeModal">ADD New Entry</button>
           </form>
-          <h3 class="font-bold text-xl">Hello!</h3>
-          <p class="py-4">Press ESC key or click on ✕ button to close</p>
         </div>
       </dialog>
     </div>
@@ -30,3 +34,4 @@
   </script>
   
   <style> </style>
+  
