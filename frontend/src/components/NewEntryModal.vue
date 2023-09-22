89bce8c9775a8706c9cae4ae059c9c66b2051caf
mjc5 <masoncooper85@gmail.com>
2023-09-22 19:35:38 -0400

    Bam!

--- frontend/src/components/NewEntryModal.vue
+++ frontend/src/components/NewEntryModal.vue
@@ -2,15 +2,21 @@
     <div>
       <button class="btn btn-ghost btn-circle" @click="openModal">+</button>
       <dialog id="my_modal_3" class="modal">
-        <div class="modal-box">
-          <form method="dialog" class="content-center">
+        <div class="modal-box flex flex-col items-center justify-center min-h-[50vh] max-h-[80vh]">
+          <form method="dialog" class="text-center">
             <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" @click="closeModal">✕</button>
             <h3 class="font-bold text-xl">Hello!</h3>
             <p class="py-4">A Curse upon Your Bloodline</p>
-            <VueDatePicker :enable-time-picker="false" :max-date="new Date()" dark />
-            <v-select multiple taggable push-tags :options="allArtists" v-model="artists" class="text-primary-content rounded">Search or add an Artist</v-select>
-            <v-select taggable push-tags :options="allVenues" class="text-primary-content rounded">Search or add an Venue</v-select>
-            <button class="btn btn-primary mt-4 " @click="closeModal">ADD New Entry</button>
+            <div class="mb-4">
+              <VueDatePicker :enable-time-picker="false" :max-date="new Date()" class="bg-primary rounded" dark />
+            </div>
+              <div class="mb-4">
+              <v-select multiple taggable push-tags :options="allArtists" v-model="artists" class="bg-primary rounded">Search or add an Artist</v-select>
+            </div>
+            <div class="mb-4">
+              <v-select taggable push-tags :options="allVenues" class="bg-primary rounded">Search or add a Venue</v-select>
+            </div>            
+            <button class="btn btn-primary mt-4" @click="closeModal">ADD New Entry</button>
           </form>
         </div>
       </dialog>
