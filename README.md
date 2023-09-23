ce29cc464399fbbe4c5341c7b8d97e97931b6ca1
Bennett Yardley <33298178+bennettyardley@users.noreply.github.com>
2023-09-23 10:47:37 -0400

    Add API Routes and Update Naming

--- README.md
+++ README.md
@@ -1,2 +1,147 @@
 # echo
 A personal concert log
+
+Features:
+
+1. Enter a concert
+   1. Multiple Artists
+   2. Venue
+   3. Date
+   4. Comment
+   5. Rating
+   6. Favorite
+   7. Pictures / Videos
+2. Stats
+3. Share (Looks like a ticket)
+4. Calendar View?
+5. Setlist?
+
+Pages:
+
+Home
+    Stats
+    Log of concerts
+    Top artists and venues
+Entry Modal
+    Artist(s)
+    Venue
+    Date
+Entry Page
+    Artist(s)
+    Venue
+    Date
+    Comment
+    Upload pictures / videos
+    Rate
+    Favorite
+    Share
+Venue
+    Times visited
+    All the concerts at that venue
+Artist
+    Times seen
+    All the times youve seen that artist
+
+entry: {
+    id str,
+    artists [str],
+    venue str,
+    date date,
+    comment str,
+    media [str],
+    rating int,
+    favorite bool,
+}
+
+/artists
+R
+{
+    artists [str]
+}
+
+/venues
+R
+{
+    venues [str]
+}
+
+/artist/:artist
+R
+{
+    entries {favorite, artists, venue, date, id},
+    media [str]
+}
+
+/venue/:venue
+R
+{
+    entries {favorite, artists, venue, date, id},
+    media [str]
+}
+
+/stats
+R - json of stats
+{
+    year: {num, pct},
+    unique: {num, pct}
+}
+
+/top
+R - json of top 5 artists and venues
+in: {
+    range str
+}
+out: {
+    media str,
+    times str,
+    artist str,
+} 
+
+/entries
+R - json of last 10 entries (paginated)
+in: {
+    page int
+}
+out: {
+    favorite bool,
+    artists str (comma seperated),
+    venue str,
+    date str (formatted as x y ago),
+    id str,
+}
+
+
+/entry
+C
+{
+    artists [str]
+    venue str
+    date date
+}
+R
+{
+    id str
+}
+{
+    artists [str],
+    venue str,
+    date str,
+    comment str,
+    media [str],
+    rating int,
+    favorite bool,
+}
+U 
+{
+    artists [str],
+    venue str,
+    date str,
+    comment str,
+    media [str],
+    rating int,
+    favorite bool, 
+}
+D
+{
+    id str
+}
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,195 @@
-const express = require("express")
-const { Deta } = require("deta")
+const express = require('express')
+const { Deta } = require('deta')
 
 const app = express()
+// eslint-disable-next-line no-undef
 const port = process.env.PORT || 8080
 
 const deta = Deta()
-const db = deta.Base("echo")
+const db = deta.Base('echo')
 
-app.get("/", (req, res) => {
-  res.send("Hello from Space! 🚀")
+function d2s(date) {
+  const currentDate = new Date()
+  const inputDateTime = date.getTime()
+  const currentDateTime = currentDate.getTime()
+  const timeDifference = currentDateTime - inputDateTime
+
+  const minute = 60 * 1000
+  const hour = 60 * minute
+  const day = 24 * hour
+  const week = 7 * day
+  const month = 30 * day
+  const year = 365 * day
+
+  if (timeDifference < week) {
+    const daysAgo = Math.floor(timeDifference / day)
+    return `${daysAgo} day${daysAgo !== 1 ? 's' : ''} ago`
+  } else if (timeDifference < month) {
+    const weeksAgo = Math.floor(timeDifference / week)
+    return `${weeksAgo} week${weeksAgo !== 1 ? 's' : ''} ago`
+  } else if (timeDifference < year) {
+    const monthsAgo = Math.floor(timeDifference / month)
+    return `${monthsAgo} month${monthsAgo !== 1 ? 's' : ''} ago`
+  } else {
+    const yearsAgo = Math.floor(timeDifference / year)
+    return `${yearsAgo} year${yearsAgo !== 1 ? 's' : ''} ago`
+  }
+}
+
+function yearStats(entries) {
+  const currentYear = new Date().getFullYear()
+
+  const currentYearEntries = entries.filter((entry) => {
+    const entryDate = new Date(entry.date)
+    return entryDate.getFullYear() === currentYear
+  })
+
+  const previousYearEntries = entries.filter((entry) => {
+    const entryDate = new Date(entry.date)
+    return entryDate.getFullYear() === currentYear - 1
+  })
+
+  const currentYearCount = currentYearEntries.length
+  const previousYearCount = previousYearEntries.length
+
+  const percentChange = previousYearCount !== 0 ? ((currentYearCount - previousYearCount) / previousYearCount) * 100 : 0
+
+  const uniqueArtistsCurrentYear = new Set()
+  currentYearEntries.forEach((entry) => {
+    if (entry.artist) uniqueArtistsCurrentYear.add(entry.artist)
+  })
+
+  const uniqueArtistsPreviousYear = new Set()
+  previousYearEntries.forEach((entry) => {
+    if (entry.artist) uniqueArtistsPreviousYear.add(entry.artist)
+  })
+
+  const currentYearUniqueCount = uniqueArtistsCurrentYear.length
+  const previousYearUniqueCount = uniqueArtistsPreviousYear.length
+
+  const uniquePercentChange =
+    previousYearUniqueCount !== 0 ? ((currentYearUniqueCount - previousYearUniqueCount) / previousYearUniqueCount) * 100 : 0
+
+  return {
+    currentYearCount,
+    previousYearCount,
+    percentChange,
+    unique: uniqueArtistsCurrentYear.size,
+    uniquePercent: uniquePercentChange,
+  }
+}
+
+function calculateTopEntries(entries, artistName, dateRange) {
+  // Get the current date
+  const currentDate = new Date()
+
+  // Define time intervals in milliseconds for last year and last decade
+  const oneYear = 365 * 24 * 60 * 60 * 1000
+  const oneDecade = 10 * oneYear
+
+  // Filter entries by artist name
+  const filteredEntries = entries.filter((entry) => {
+    return entry.artist === artistName
+  })
+
+  // Filter entries by date range
+  const filteredEntriesByDate = filteredEntries.filter((entry) => {
+    const entryDate = new Date(entry.date)
+
+    switch (dateRange) {
+      case 'allTime':
+        return true // No date filter for all time
+      case 'lastYear':
+        return currentDate - entryDate <= oneYear
+      case 'lastDecade':
+        return currentDate - entryDate <= oneDecade
+      default:
+        return false
+    }
+  })
+
+  // Sort entries by a relevant metric (e.g., by date or another field)
+  // Replace 'date' with the actual field you want to sort by
+  filteredEntriesByDate.sort((a, b) => {
+    const dateA = new Date(a.date)
+    const dateB = new Date(b.date)
+    return dateB - dateA // Sort in descending order by date
+  })
+
+  // Get the top 5 entries
+  const topEntries = filteredEntriesByDate.slice(0, 5)
+
+  return topEntries
+}
+
+app.get('/artists', async (req, res) => {
+  const allEntries = await db.fetch()
+  let artists = []
+  for (let entry in allEntries.items) {
+    artists.concat(entry.artists)
+  }
+  res.json(artists)
+})
+
+app.get('/venues', async (req, res) => {
+  const allEntries = await db.fetch()
+  let venues = []
+  for (let entry in allEntries.items) {
+    venues.push(entry.venue)
+  }
+  res.json(venues)
+})
+
+app.get('/artist/:artist', async (req, res) => {
+  const entries = await db.fetch({ 'artists?contains': req.params.artist })
+  let formattedEntries = []
+  let media = []
+  for (let entry in entries.items) {
+    formattedEntries.push({
+      favorite: entry.favorite,
+      artists: entry.artists.join(', '),
+      venue: entry.venue,
+      date: d2s(entry.date),
+      id: entry.key,
+    })
+  }
+  res.json({ entries: formattedEntries, media })
+})
+
+app.get('/venue/:venue', async (req, res) => {
+  const entries = await db.fetch({ venue: req.params.venue })
+  let formattedEntries = []
+  let media = []
+  for (let entry in entries.items) {
+    formattedEntries.push({
+      favorite: entry.favorite,
+      artists: entry.artists.join(', '),
+      venue: entry.venue,
+      date: d2s(entry.date),
+      id: entry.key,
+    })
+  }
+  res.json({ entries: formattedEntries, media })
+})
+
+app.get('/stats', async (req, res) => {
+  const allEntries = await db.fetch()
+  const stats = yearStats(allEntries)
+
+  res.json({
+    year: {
+      num: `${stats.currentYearCount}`,
+      pct: `${stats.percentChange.toFixed(2)}%`,
+    },
+    unique: {
+      num: `${stats.unique}`,
+      pct: `${stats.uniquePercent.toFixed(2)}%`,
+    },
+  })
+})
+
+app.get('/top', async (req, res) => {
+  const allEntries = await db.fetch()
 })
 
 app.listen(port, () => {
